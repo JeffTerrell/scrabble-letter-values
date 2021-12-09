fba4ddee89eca5b3558d97b76d7b1ed5598a2ec2
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-09 13:57:07 -0500

    add passing logic for test 4 and create letterValue dict
    Co-authored by: Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored-by: Hannah Young <hannah@corgibyte.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- Scrabble/Models/ScrabbleLetter.cs
+++ Scrabble/Models/ScrabbleLetter.cs
@@ -1,4 +1,4 @@
-
+using System.Collections.Generic;
 
 namespace Scrabble.Models
 {
@@ -14,11 +14,58 @@ namespace Scrabble.Models
 
     public int GetScore()
     {
-      //TODO
-      return 0;
+      Dictionary<char, int> letterValues = new Dictionary<char, int>() 
+      { 
+        {'a', 1},
+        {'e', 1},
+        {'i', 1},
+        {'o', 1},
+        {'u', 1},
+        {'l', 1},
+        {'n', 1},
+        {'r', 1},
+        {'s', 1},
+        {'t', 1},
+        {'d', 2},
+        {'g', 2},
+        {'b', 3},
+        {'c', 3},
+        {'m', 3},
+        {'p', 3},
+        {'f', 4},
+        {'h', 4},
+        {'v', 4},
+        {'w', 4},
+        {'y', 4},
+        {'k', 5},
+        {'j', 8},
+        {'x', 8},
+        {'q', 10},
+        {'z', 10}
+      };
+      int score = 0;
+      char[] wordInChars = Word.ToCharArray();
+      foreach (char letter in wordInChars)
+      {
+        score += letterValues[letter];
+      }
+      return score;
     }
   }
 }
+
+    // foreach (string groceryItem in myGroceryList)
+    // {
+    //   Console.WriteLine(groceryItem);
+    // }
+
+// A, E, I, O, U, L, N, R, S, T       1
+// D, G                               2
+// B, C, M, P                         3
+// F, H, V, W, Y                      4
+// K                                  5
+// Q, Z                               10
+
 
 
 // User prompt to enter a word
@@ -27,3 +74,4 @@ namespace Scrabble.Models
 // method : loop through the dictionary field and add to a totalScore when theres a match?
 // Constructor : create instance with word stored
 
+// > Dictionary<string, string> myDictionary = new Dictionary<string, string>() { {"A", "apple"}, {"B", "bear"} };
